fe54a7a895022e122ff1030c3b390b01228316a1
PascalUlor <pascalulor@yahoo.com>
2019-05-27 16:56:16 +0100

    ft(refactor footer component)

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner';
 import CardContent from './CardContent';
-import Footer from '../FooterComponents/Footer';
+// import Footer from '../FooterComponents/Footer';
 
 const nextPage = () => window.location.href = 'https://reactjs.org';
 
 const imageUrl = `https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png`;
 function CardContainer () {
    return (
-   <div onClick={nextPage} className='card'>
-    <CardBanner url={imageUrl}/>
-    <CardContent />
-    <Footer />
-    </div>);
+    <div onClick={nextPage} className='card'>
+        <CardBanner url={imageUrl}/>
+        <CardContent />
+    {/* <Footer /> */}
+    </div>
+ );
 }
 
 export default CardContainer;
