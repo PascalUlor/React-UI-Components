1f6a5e5e1a032942710352bbb1df474a88f894e2
PascalUlor <pascalulor@yahoo.com>
2019-05-28 14:20:56 +0100

    ft(create number buttons and layout for symbols)

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 import './App.css';
 
 const App = () => {
   return (
     <div className='main__container'>
       <CalculatorDisplay />
-      <div className='action__container'>
-      <ActionButton text='clear'/>
-      <ActionButton text='0'/>
+      <div className='button__container'>
+        <div className='action__container'>
+          <ActionButton text='clear' />
+          <NumberButton digit='7' />
+          <NumberButton digit='8' />
+          <NumberButton digit='9' />
+          <NumberButton digit='4' />
+          <NumberButton digit='5' />
+          <NumberButton digit='6' />
+          <NumberButton digit='1' />
+          <NumberButton digit='2' />
+          <NumberButton digit='3' />
+          <ActionButton text='0' />
+        </div>
+        <div className='symbols'></div>
       </div>
 
       {/* <h3>Welcome to React Calculator</h3>
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+export default function NumberButton ({digit}) {
+    return (
+    <div className='number__buttons'>
+        <p>{digit}</p>
+    </div>
+    );
+}
