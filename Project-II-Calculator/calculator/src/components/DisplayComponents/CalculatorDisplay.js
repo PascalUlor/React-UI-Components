c1156dd35da7289e62e18c858409c41fc9c44dcd
PascalUlor <pascalulor@yahoo.com>
2019-05-28 12:55:52 +0100

    ft(create calculator display)

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+export default function CalculatorDisplay () {
+    return (
+    <div className='calculator__display'>
+        <p>0</p>
+    </div>
+        );
+}
